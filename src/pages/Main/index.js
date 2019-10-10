15d887f461543a9b23fa25395c3ce53df2940cd0
LORDBABUINO <jorge.x7@gmail.com>
2019-10-09 22:27:19 -0300

    Gets data from github API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Keyboard } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import api from '../../services/api'
 import { Container, Form, Input, SubmitButton } from './styles'
 
-const Main = () => (
-  <Container>
-    <Form>
-      <Input
-        autoCorrect={false}
-        autoCapitalize="none"
-        placeholder="Adicionar usuário"
-      />
-      <SubmitButton>
-        <Icon name="add" size={20} color="white" />
-      </SubmitButton>
-    </Form>
-  </Container>
-)
+class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  }
+
+  handleAddUser = async () => {
+    const { newUser, users } = this.state
+
+    const {
+      data: { name, login, bio, avatar_url: avatar },
+    } = await api.get(`/users/${newUser}`)
+
+    const data = { name, login, bio, avatar }
+    this.setState({ users: [...users, data], newUser: '' })
+    Keyboard.dismiss()
+  }
+
+  render() {
+    const { newUser } = this.state
+    return (
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({ newUser: text })}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={20} color="white" />
+          </SubmitButton>
+        </Form>
+      </Container>
+    )
+  }
+}
 
 Main.navigationOptions = {
   title: 'Usuários',
