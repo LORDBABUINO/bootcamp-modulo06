1e78c6704d8e80ec31dbfac39df33194ca0e366f
LORDBABUINO <jorge.x7@gmail.com>
2019-10-11 17:56:27 -0300

    Builds user page

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { Keyboard, ActivityIndicator } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage'
 import Icon from 'react-native-vector-icons/MaterialIcons'
@@ -53,6 +54,12 @@ class Main extends Component {
     Keyboard.dismiss()
   }
 
+  handleNavigate = user => async () => {
+    const { navigation } = this.props
+
+    navigation.navigate('User', { user })
+  }
+
   render() {
     const { newUser, users, loading } = this.state
     return (
@@ -83,7 +90,7 @@ class Main extends Component {
               <Avatar source={{ uri: item.avatar }} />
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -97,4 +104,11 @@ class Main extends Component {
 Main.navigationOptions = {
   title: 'Usuários',
 }
+
+Main.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
+}
+
 export default Main
